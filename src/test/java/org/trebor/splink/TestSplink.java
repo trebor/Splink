fc32a00e73ddc59c6b914db107f1e5041baee5d0
Robert Harris <github.com@trebor.org>
2011-03-08 08:32:44 -0800

    renamed unit test file

--- src/test/java/org/trebor/splink/TestSplink.java
+++ src/test/java/org/trebor/splink/TestSplink.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 import org.junit.Test;
 import org.trebor.splink.Splink.ResourceType;
 
-public class SplinkTests
+public class TestSplink
 {
   @Test
   public void testUri()
